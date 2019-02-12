f3cf3bb70f228c30db5244721f244e4a112125ce
HelloWorldIL <idobronfeld@gmail.com>
2019-02-12 14:49:49 +0200

    added about page card element as dynamic component

---
+++ client/src/components/imgInfoCard.vue
@@ -0,0 +1,59 @@
+<template lang="pug">
+  .imgInfoContainer
+    v-card.imgInfoTitle(v-bind:style="{ 'background-image': 'url(' + backgroundImage +')'}")
+      h1 {{title}}
+    v-card.imgInfoText
+      p {{text}}
+</template>
+
+<script>
+export default {
+  props: {
+    backgroundImage:{
+      type: String,
+      default: "/static/img.jpg"
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    text: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+
+<style lang="stylus" scoped>
+.imgInfoContainer
+  display grid
+  margin-top 50px
+  grid-template-columns 100px 1fr 1fr 100px
+  grid-template-areas:
+    ". title text ."
+
+  > .imgInfoTitle
+    grid-area title
+    background-size cover
+    display flex
+    align-items center
+    justify-content center
+    color white
+    padding 50px
+    > h1
+      display inline-block
+      vertical-align middle
+  > .imgInfoText
+    grid-area text
+    padding 0px 50px
+
+
+@media only screen and (max-width: 1200px) {
+  .imgInfoContainer {
+    grid-template-areas:
+    ". title title ." ". text text .";
+  }
+}
+</style>
--- client/src/views/about.vue
+++ client/src/views/about.vue
@@ -2,50 +2,23 @@
 v-app()
   Parallax(background-image="/static/cropped.JPG" height="40vh")
     h1 About Us
-  .aboutSection
-    v-card.aboutTitle
-      h1 We Build Satellites
-    v-card.aboutText
-      p Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+  section
+    H1 This is what we do
+  imgInfoCard(title="We Build Satellites", background-image="/static/20180913_194503.jpg" :text="cardText")
 </template>
 
 <script>
 import Parallax from '../components/homeParallax.vue'
+import imgInfoCard from '../components/imgInfoCard.vue'
 export default {
   components:{
-    Parallax
+    Parallax,
+    imgInfoCard
+  },
+  data: function() {
+    return{
+      cardText: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    }
   }
 }
 </script>
-
-<style lang="stylus" scoped>
-.aboutSection
-  display grid
-  margin-top 50px
-  grid-template-columns 100px 1fr 1fr 100px
-  grid-template-areas:
-    ". title text ."
-
-  > .aboutTitle
-    grid-area title
-    display flex
-    background #212121
-    align-items center
-    justify-content center
-    color white
-    padding 50px
-    > h1
-      display inline-block
-      vertical-align middle
-  > .aboutText
-    grid-area text
-    padding 0px 50px
-
-
-@media only screen and (max-width: 1200px) {
-  .aboutSection {
-    grid-template-areas:
-    ". title title ." ". text text .";
-  }
-}
-</style>
